be610247a0cf7d37ad2802f91533d76d9ef7a128
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2023-08-27 19:25:29 -0300

    routes: add /json

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
-use axum::{extract::ConnectInfo, routing::get, Router};
 use std::net::{SocketAddr, IpAddr, Ipv6Addr};
+
+use axum::{
+    extract::ConnectInfo, http::StatusCode, response::IntoResponse, routing::get, Json, Router,
+};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Data {
+    ip: String,
+    port: u16,
+}
 
 #[tokio::main]
 async fn main() {
@@ -14,12 +24,24 @@ async fn main() {
         .unwrap();
 }
 
-
 fn app() -> Router {
-    Router::new().route("/", get(root))
+    Router::new()
+        .route("/", get(root))
+        .route("/json", get(json))
 }
 
 async fn root(ConnectInfo(addr): ConnectInfo<SocketAddr>) -> String {
     tracing::info!("GET(/) Handling connection from {}", addr);
     format!("Hello, {}", addr.ip())
+}
+
+async fn json(ConnectInfo(addr): ConnectInfo<SocketAddr>) -> impl IntoResponse {
+    tracing::info!("GET(/json) Handling connection from {}", addr);
+
+    let connection_data = Data {
+        ip: addr.ip().to_string(),
+        port: addr.port(),
+    };
+
+    (StatusCode::OK, Json(connection_data))
 }

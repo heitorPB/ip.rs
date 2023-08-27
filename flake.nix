6ae83d570a471a71de3d568739b25b7f4b6fd6d9
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2023-08-27 17:29:44 -0300

    Flake: use naersk and build OCI Image

--- flake.nix
+++ flake.nix
@@ -2,19 +2,40 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+  outputs = { self, nixpkgs, flake-utils, naersk }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        naersk' = pkgs.callPackage naersk { };
       in
       rec {
+        packages = rec {
+          # For `nix build .#ip_rs`
+          ip_rs = naersk'.buildPackage {
+            src = ./.;
+          };
+
+          # For `nix build .#ociImage` and then `podman image load ./result`
+          ociImage = pkgs.dockerTools.buildLayeredImage {
+            name = "registry.fly.io/iprs";
+            tag = "latest";
+
+            contents = [ pkgs.coreutils ip_rs ];
+            config = {
+              Cmd = [ "${ip_rs}/bin/ip-rs" ];
+            };
+          };
+        };
+
         # For `nix develop`
         devShell = pkgs.mkShell rec {
-          buildInputs = with pkgs; [
+          nativeBuildInputs = with pkgs; [
             cargo
             clippy
+            flyctl
             rust-analyzer
             rustc
             rustfmt
